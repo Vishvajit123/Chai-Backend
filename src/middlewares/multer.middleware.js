ad0d0cececa0903c487f950a772418f8739e101f
vishvajit <wanivishvajit1234@gmail.com>
2024-09-16 01:24:06 +0530

    Add Controllers and Bug Fixes for Register User

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,11 +1,17 @@
+// multer: This is a middleware for handling multipart/form-data, which is used for uploading files. It can handle file uploads through forms in web applications.
 import multer from "multer";
 
+// multer.diskStorage() is used to control how files are stored on the disk (file system) when uploaded.
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
+    // destination: A function that determines where the uploaded files should be stored.
+    // req: The request object.
+    //  file: The uploaded file’s object.
+    //  cb: A callback function (cb(null, path)) that you use to specify the storage location. 
+  destination: function (req, file, cb) {
       cb(null, "./public/temp")
     },
+    // filename: A function that determines the name of the uploaded file on the server.
     filename: function (req, file, cb) {
-
         cb(null, file.originalname)
     }
   })
